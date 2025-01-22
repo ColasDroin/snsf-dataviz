2946214735f8a949b33bac61a2d38aac516d10c9
Colas Droin <colasdroin@gmail.com>
2025-01-22 15:13:20 +0100

    Better initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,10 @@ import { data } from "@/../public/data/data";
 import circles2024 from "@/../public/data/grant_2024_circles.json";
 import { Barplot } from "@/components/charts/barplot/Barplot";
 import { ResponsiveCross } from "@/components/charts/cross/Cross";
-import { ScrollContainer } from "@/components/ScrollContainer";
+import { ScrollContainer } from "@/components/scrollContainer";
 export default function Home() {
   return (
     <main>
-      <div className="w-full h-screen bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-red-800 to-red-600 flex items-center justify-center text-white mb-8 flex flex-col items-center justify-center ">
-        <p className="mt-9 mb-8 text-sm md:text-4xl font-bold text-center">
-          How is Swiss research funded?
-        </p>
-        <div className="h-full max-h-[50vh] max-w-full aspect-square">
-          <ResponsiveCross data={circles2024} />
-        </div>
-        <p className="mb-9 mt-8 text-sm md:text-4xl font-bold text-center">
-          A visual story about the Swiss National Science Foundation
-        </p>
-      </div>
-
-      <div
-        className="container mx-auto px-4 py-6 "
-        style={{ maxWidth: 900 }}
-      ></div>
       <ScrollContainer />
       <div className="container mx-auto px-4 py-6 " style={{ maxWidth: 900 }}>
         <h3 className="text-red-800 text-center mb-[100vh]">
--- src/components/ScrollContainer.tsx
+++ src/components/ScrollContainer.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Scrollama, Step } from "react-scrollama";
+import circles2024 from "@/../public/data/grant_2024_circles.json";
+import { ResponsiveCross } from "@/components/charts/cross/Cross";
 
 export const ScrollContainer = () => {
   const [currentStepIndex, setCurrentStepIndex] = useState(null);
@@ -11,18 +13,27 @@ export const ScrollContainer = () => {
   };
 
   return (
-    <div style={{ margin: "50vh 0", border: "2px dashed skyblue" }}>
+    // <div style={{ margin: "0vh 0", border: "2px dashed skyblue" }}>
+    <div className="w-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-red-800 to-red-600 flex items-center justify-center text-white mb-8 flex flex-col items-center justify-center ">
+      <p className="text-sm md:text-4xl font-bold text-center h-[15vh] flex items-center justify-center">
+        How is Swiss research funded?
+      </p>
       <div
-        style={{ position: "sticky", top: "30vh", border: "1px solid orchid" }}
+        className="h-[50vh] max-w-full aspect-square"
+        style={{ position: "sticky", top: "15vh", border: "1px solid orchid" }}
       >
-        I'm sticky. The current triggered step index is: {currentStepIndex}
+        <ResponsiveCross data={circles2024} />
       </div>
-      <Scrollama offset={0.2} onStepEnter={onStepEnter} debug>
+      <p className="text-sm md:text-4xl font-bold text-center h-[15vh] flex items-center justify-center">
+        A visual story about the Swiss National Science Foundation
+      </p>
+      {/* </div> */}
+      <Scrollama offset={0.8} onStepEnter={onStepEnter} debug>
         {[1, 2, 3, 4].map((_, stepIndex) => (
           <Step data={stepIndex} key={stepIndex}>
             <div
               style={{
-                margin: "50vh 0",
+                margin: "0vh 0",
                 border: "1px solid gray",
                 opacity: currentStepIndex === stepIndex ? 1 : 0.2,
                 height: 200,
